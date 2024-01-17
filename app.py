aaad704d5ee09a79d104da29b5c94687a04fb681
stevephary <stevephary@gmail.com>
2024-01-17 22:46:08 +0300

    haa

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 from supabase import create_client, Client
+
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
@@ -17,6 +19,13 @@ def run_query():
 
 rows = run_query()
 
-# Print results.
-for row in rows.data:
-    st.write(f"{row['productname']}")
+# Convert the result to a Pandas DataFrame.
+df = pd.DataFrame(rows.data)
+
+# Sidebar
+st.sidebar.title("ADONAI SHOP")
+show_data_button = st.sidebar.button("Manunuzi ya bidhaa")
+
+# Display the Pandas DataFrame using Streamlit table based on the button click.
+if show_data_button:
+    st.write(df)
